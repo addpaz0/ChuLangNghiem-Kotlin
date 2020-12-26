c1baa25679663677554058298db0fd6705e476ac
Phat Huynh <addpaz0dev@gmail.com>
2020-12-26 20:07:38 +0700

    2.1

--- app/src/main/java/com/example/chulangnghiem/MainActivity.kt
+++ app/src/main/java/com/example/chulangnghiem/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.example.chulangnghiem
 
+import android.app.Activity
 import android.content.ActivityNotFoundException
 import android.content.DialogInterface
 import android.content.Intent
+import android.media.MediaPlayer
 import android.net.Uri
 import android.os.Bundle
 import android.view.View
+import android.webkit.WebView
 import android.widget.Switch
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
@@ -14,54 +17,72 @@ import com.example.chulangnghiem.databinding.ActivityMainBinding
 import kotlinx.android.synthetic.main.activity_main.*
 
 
-open class MainActivity : AppCompatActivity() {
+class MainActivity : Activity() {
+
+    private lateinit var mp: MediaPlayer
+    var check_st: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-//        val binding = ActivityMainBinding.inflate(layoutInflater)
-//        setContentView(binding.root)
+        // Media Player
+        mp = MediaPlayer.create(this, R.raw.chulangnghiem)
+        mp.isLooping = true
+        mp.setVolume(0.5f, 0.5f)
 
+//        //Full Screen hãy thay đổi  AppCompatActivity thành Activity
+//        requestWindowFeature(Window.FEATURE_NO_TITLE)
+//        window.setFlags(
+//            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+//            WindowManager.LayoutParams.FLAG_FULLSCREEN
+//        )
         setContentView(R.layout.activity_main)
-        xuLyWeb()
+        xulyWebView()
+
 
         topAppBar.setNavigationOnClickListener {
             // Handle navigation icon press
         }
-
-           topAppBar.setOnMenuItemClickListener { menuItem ->
+        topAppBar.setOnMenuItemClickListener { menuItem ->
             when (menuItem.itemId) {
-                R.id.mn_menu -> {
-                    val urlnight: String = "file:///android_asset/CHULANGNGHIEMNIGHT.htm"
-                    myWeb.loadUrl(urlnight)
-                    true
-                }
-                R.id.mn_toi ->{
-                    val url: String = "file:///android_asset/CHULANGNGHIEM.htm"
-                    myWeb.loadUrl(url)
-                    true
-                }
-                R.id.menu_gioiThieu ->{
-                    startActivity(Intent(this,GioiThieu::class.java))
+
+                R.id.menuMp3 ->
+                {
+                    if (mp.isPlaying) {
+                        menuItem.isChecked
+                        // Stop
+                        mp.pause()
+                        menuItem.setIcon(R.drawable.ic_baseline_music_note_24)
+                    } else {
+                        // Start
+                        mp.start()
+                        menuItem.setIcon(R.drawable.ic_baseline_music_off_24)
+                    }
                     true
                 }
 
-                R.id.menu_khac -> {
-                    try {
-                        startActivity(
-                            Intent(
-                                Intent.ACTION_VIEW,
-                                Uri.parse("https://play.google.com/store/search?q=pub%3APhat93&c=apps")
-                            )
-                        )
-                    } catch (e: ActivityNotFoundException) {
-                        startActivity(
-                            Intent(
-                                Intent.ACTION_VIEW,
-                                Uri.parse("https://play.google.com/store/search?q=pub%3APhat93&c=apps")
-                            )
-                        )
+                R.id.menuST ->  {
+                    if(menuItem.isChecked){
+                        menuItem.setIcon(R.drawable.ic_sun)
+                    } else if(check_st == false){
+                        menuItem.isChecked
+                        check_st = true
+                        val urln: String = "file:///android_asset/CHULANGNGHIEMNIGHT.htm"
+                        val myWebView: WebView = findViewById(R.id.myWeb)
+                        myWebView.loadUrl(urln)
+                        menuItem.setIcon(R.drawable.ic_sun)
+                    }else if (check_st == true) {
+                        check_st = false
+                        val urln: String = "file:///android_asset/CHULANGNGHIEM.htm"
+                        val myWebView: WebView = findViewById(R.id.myWeb)
+                        myWebView.loadUrl(urln)
+                        menuItem.setIcon(R.drawable.ic_night)
                     }
+                    true
+                }
+
+                R.id.menuBook -> {
+                    startActivity(Intent(this, GioiThieu::class.java))
                     true
                 }
                 else -> false
@@ -70,11 +91,12 @@ open class MainActivity : AppCompatActivity() {
 
     }
 
-    private fun xuLyWeb() {
-        val url: String = "file:///android_asset/CHULANGNGHIEM.htm"
-        myWeb.loadUrl(url)
+    private fun xulyWebView() {
+        //Load WebView
+        var url: String = "file:///android_asset/CHULANGNGHIEM.htm"
+        var myWebView: WebView = findViewById(R.id.myWeb)
+        myWebView.loadUrl(url)
     }
-
     //Tao phuong thuc AlerDialog va nhan phim back quay ve
     override fun onBackPressed() {
         val builder: AlertDialog.Builder = AlertDialog.Builder(this)
@@ -83,6 +105,7 @@ open class MainActivity : AppCompatActivity() {
         builder.setMessage("Thí chủ muốn xuống núi?")
         builder.setPositiveButton("Mệt Nghỉ", DialogInterface.OnClickListener { dialog, which ->
             finish()
+            mp.stop()
         })
         builder.setNegativeButton("Tới bến", DialogInterface.OnClickListener { dialog, which ->
             dialog.cancel()
@@ -91,10 +114,7 @@ open class MainActivity : AppCompatActivity() {
         alertDialog.show()
     }
 
-
-
 }
-
 
 
 
